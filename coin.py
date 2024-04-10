7984b28ce8c88903daae98e2dd555a7be6658daa
spy35 <spy35@bible.ac.kr>
2024-04-10 17:57:10 +0900

    first commit

---
+++ coin.py
@@ -0,0 +1,16 @@
+import random
+
+    # 앞면과 뒷면의 횟수를 저장할 변수를 초기화합니다.
+front = 0
+back = 0
+
+for _ in range(100):
+    # 0 또는 1을 무작위로 생성
+    coin = random.randint(0, 1)
+
+    if coin == 0:
+        front += 1
+    else:
+        back += 1
+
+print("동전의 앞면은 {}번 뒷면은 {}번".format(front, back))
